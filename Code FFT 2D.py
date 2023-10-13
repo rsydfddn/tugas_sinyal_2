a2b62e9c35c915df8c656c9c2713c0ac1d318f56
Muhammad Rasyid Fiddiin <139645166+rsydfddn@users.noreply.github.com>
2023-10-13 21:32:06 +0800

    Add files via upload

--- Code FFT 2D.py
+++ Code FFT 2D.py
@@ -2,38 +2,33 @@ import cmath
 import math
 import matplotlib.pyplot as plt
 
-def fft2d(matrix):
-    # Menghitung FFT dalam dimensi pertama (baris)
-    rows = [fft(row) for row in matrix]
-    
-    # Menghitung FFT dalam dimensi kedua (kolom)
-    cols = [fft([row[i] for row in rows]) for i in range(len(rows[0]))]
-    
-    return cols
-
 def fft(signal):
     N = len(signal)
     if N <= 1:
         return signal
+    
     even = fft(signal[0::2])
     odd = fft(signal[1::2])
+    
     T = [cmath.exp(-2j * math.pi * k / N) * odd[k] for k in range(N // 2)]
-    return [even[k] + T[k] for k in range(N // 2)] + \
-           [even[k] - T[k] for k in range(N // 2)]
+    
+    return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
+
+def fft2d(data):
+    # FFT pada setiap baris
+    rows = [fft(row) for row in data]
+    # FFT pada setiap kolom
+    cols = [fft([row[i] for row in rows]) for i in range(len(data[0]))]
+    return cols
 
 # Contoh penggunaan FFT 2D
-if _name_ == "_main_":
-    # Contoh matriks 2D (citra)
-    matrix = [
-        [0, 1, 2, 3],
-        [4, 5, 6, 7],
-        [8, 9, 10, 11],
-        [12, 13, 14, 15]
-    ]
+if __name__ == "__main__":
+    # Contoh data dua dimensi
+    data = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
     
-    # Menghitung FFT 2D
-    fft_result = fft2d(matrix)
-    
+    # Menghitung FFT dua dimensi
+    fft_result = fft2d(data)
+
     # Menampilkan hasil FFT
     print("FFT Result:")
     for row in fft_result:
@@ -42,6 +37,4 @@ if _name_ == "_main_":
     # Membentuk grafik dari hasil FFT
     magnitude_spectrum = [[abs(x) for x in row] for row in fft_result]
     plt.imshow(magnitude_spectrum, cmap='gray')
-    plt.title('Magnitude Spectrum (FFT Result)')
-    plt.colorbar()
     plt.show()
